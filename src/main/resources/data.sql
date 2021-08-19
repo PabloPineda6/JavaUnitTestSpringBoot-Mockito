e17144e0718e154a5a5cd916407a137ee0a8ab0c
DESKTOP-LIMJE25\Pablo Pineda <ppineda@lean-tech.io>
2021-08-18 20:52:24 -0500

    following tutorial until chapter 35, DB creation was unsuccessful

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
--- insert into item(id, name, price, quantity) values(10001, 'cylinder', 10, 20);
+ insert into item(id, name, price, quantity) values(10001, 'cylinder', 10, 20);
+ insert into item(id, name, price, quantity) values(10002, 'cone', 12, 50);
+ insert into item(id, name, price, quantity) values(10003, 'disc', 15, 40);
